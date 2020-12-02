77fc7b5ac13752a39257cd441868bb9ca03e497e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-12-02 22:01:28 +0000

    Automatically backed up by Learn

---
+++ current_weather.rb
@@ -0,0 +1,14 @@
+current_weather = "raining"
+
+case current_weather
+
+  when "sunny"
+    puts "Grab some sunscreen!"
+    
+  when "raining"
+    puts "grab an umbrella"
+    
+  when "snowing"
+    puts "bundle up"
+    
+end
---
+++ learning-comments.rb
@@ -0,0 +1,13 @@
+
+
+# from the Three Dog Night song: "Jot to the World"(Jerimiah was a bullfrog)
+puts"Joy to the world"
+puts"All the boys and girls"
+# puts"Joy to the fishes in the deep blue sea"
+puts"Joy to you and me"
+
+name="Byron"
+# name = "Luca"
+
+puts "We're sorry, but per health inspectors rules, #{name} is not allowed in the store."
+
